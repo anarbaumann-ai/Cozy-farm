80e806331bef3246e24cd5f4d9c6d3861351dc48
anarbaumann-ai <anarbaumann@googlemail.com>
2025-08-28 20:21:08 +0200

    Add files via upload

---
+++ cozy_farm_sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'cozy-farm-v1';
+const ASSETS = [
+  './cozy_farm_mobile.html',
+  './cozy_farm_manifest.webmanifest'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE && caches.delete(k)))));
+});
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  }
+});
